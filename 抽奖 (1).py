476e29b06432d353e4997700d415edde30d21044
社会你鸿哥 <xiaoh0372@gmail.com>
2025-01-21 11:39:01 +0000

    1.2.0

--- 抽奖 (1).py
+++ 抽奖 (1).py
@@ -15,6 +15,9 @@ def callback():
     elif 31<a<=60:
         var.set("三等奖\一秒被XXS反骂")
         label2.config(fg="green")
+    elif 61<a<=100:
+        var.set("四等奖\一秒是赖清德学徒！")
+        label2.config(fg="yellow")
     else:
         var.set("未中奖\一秒是SB！")
         label2.config(fg="black")
